1377ed727f8546073d96abe0c57def7d3a4e8f89
Arthur Lozano <chillout885@gmail.com>
2020-10-16 11:06:36 -0700

    updated javascript to reflect source utilized for creating function and css to change the display, also changed html pages to link to one another

--- js/app.js
+++ js/app.js
@@ -2,30 +2,36 @@
 
 
 //Create object(s) - Number One below
-var hoursOp = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm'];
-var myCookies = ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies','144 cookies', '119 cookies','84 cookies','61 cookies','23 cookies','42 cookies','57 cookies','Total: 875 cookies'];
+var hoursOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+var myCookies = ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', 'Total: 875 cookies'];
+// First object with given data and properties
+//1. render to the DOM
+//2. the render needs to be a method of the object
+// render article with h2, p, ul with list, img
+
 var seattle = {
   name: ('Seattle'),
   minHours: 23,
   maxHours: 65,
   avgCookie: 6.3, // Simulated amount of cookies purchased
   cookiesPerHr: [],
-  totalSale:0,
-  randomCustomer: function() {
+  totalSale: 0,
+  // Helper function to get random number, code used from mdn docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomer: function () {
     return Math.floor(Math.random() * (this.maxHours - this.minHours + 1) + this.minHours);
   },
 
- CalccookiesPerHr: for (var i = 0; i < myCookies.length; i++) {
-   var li = document.createElement('li');
-   li.textContent = this.maxHours[i];
-   ul.appendChild(li);
-   const element = array[i];
+  CalccookiesPerHr: for(var i = 0; i<myCookies.length; i++) {
+    var li = document.createElement('li');
+li.textContent = this.maxHours[i];
+ul.appendChild(li);
+const element = array[i];
  }
 
- var img = document.createElement('img');
- img.setAttribute('src', 'img/salmon.png');
- img.setAttribute('alt', `Welcome to ${this.name}: Hope you like it`);
- article.appendChild(img);
+var img = document.createElement('img');
+img.setAttribute('src', 'img/salmon.png');
+img.setAttribute('alt', `Welcome to ${this.name}: Hope you like it`);
+article.appendChild(img);
 
 };
 
