9d0d39f0604adf8389c8770363ac9a323ee31b3c
Arthur Lozano <chillout885@gmail.com>
2020-10-16 22:33:47 -0700

    updated javascript functions

--- js/app.js
+++ js/app.js
@@ -5,34 +5,60 @@
 var hoursOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 var myCookies = ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', 'Total: 875 cookies'];
 // First object with given data and properties
-//1. render to the DOM
 //2. the render needs to be a method of the object
 // render article with h2, p, ul with list, img
 
 var seattle = {
-  name: ('Seattle'),
-  minHours: 23,
-  maxHours: 65,
+  name: 'Seattle',
+  min: 23,
+  max: 65,
   avgCookie: 6.3, // Simulated amount of cookies purchased
-  cookiesPerHr: [],
+  cookiesPerHrArray: [],
   totalSale: 0,
+  // Uses a method of that object to generate a random number of customers per hour.
   // Helper function to get random number, code used from mdn docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   randomCustomer: function () {
-    return Math.floor(Math.random() * (this.maxHours - this.minHours + 1) + this.minHours);
+    for (var i = 0; i < hoursOp.length; i++) {
+      var customerTotal = randomNumber(this.min, this.max);
+      var simulatedAmtCookies = math.floor(this.avgCookie * customerTotal);
+      this.totalSale += simulatedAmtCookies;
+      this.cookiesPerHrArray.push(simulatedAmtCookies);
+    }
   },
 
-  CalccookiesPerHr: for(var i = 0; i<myCookies.length; i++) {
+
+
+  //1. render to the DOM
+  var parentElement = document.getElementById('cityCookie');
+  //Shows everything within the getElement method within the console - console.log(parentElement);
+
+
+  // When creating elements there is a pattern
+  // Create element
+  // give it content
+  // append to the DOM
+  var article = document.createElement('article');
+  parentElement.appendChild(article); //Created Element and appended to the DOM which can be viewed in the browser developer window, the $0 shows it was created in Javascript not HTML
+
+  // A little easier to make variable name similar to element created
+  var h2 = document.createElement('h2');// h2 is an object
+  h2.textContent = seattle.name;//So it has properties such as textContent
+  article.appendChild(h2);
+
+  var p = document.createElement('p');
+  p.textContent = `${seattle.name} is an expensive city for cookies ${seattle.maxHours}`;
+  article.appendChild(p);
+
+  var ul = document.createElement('ul');
+  article.appendChild(ul);
+
+  //most relevant part of lab-06
+  for(var i = 0; i<hoursOp.length; i++) {
     var li = document.createElement('li');
-li.textContent = this.maxHours[i];
+li.textContent = hoursOp[i];
 ul.appendChild(li);
-const element = array[i];
- }
+}
 
-var img = document.createElement('img');
-img.setAttribute('src', 'img/salmon.png');
-img.setAttribute('alt', `Welcome to ${this.name}: Hope you like it`);
-article.appendChild(img);
 
-};
 
 
